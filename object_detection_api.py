751367979821810c939f4e2b05418b4070f1b5e9
AnhDH <anhdhbn@gmail.com>
2019-06-29 20:48:31 +0700

    fix 500

--- object_detection_api.py
+++ object_detection_api.py
@@ -219,7 +219,9 @@ def run_inference_for_single_image(image, graph, threshold):
         if scores[c] >= threshold:      # only return confidences equal or greater than the threshold
             print(" object %s - score: %s, coordinates: %s" % (class_name, scores[c], boxes[c]))
             
-            print(type(class_name), type(scores[c].shape), type(boxes[c][0].shape), type(boxes[c][1].shape), type(boxes[c][2].shape), type(boxes[c][3].shape))
+            # print(type(class_name), type(scores[c].shape), type(boxes[c][0].shape), type(boxes[c][1].shape), type(boxes[c][2].shape), type(boxes[c][3].shape))
+            print((class_name), (scores[c].shape), (boxes[c][0].shape), (boxes[c][1].shape), (boxes[c][2].shape), (boxes[c][3].shape))
+            print((class_name), (scores[c]), (boxes[c][0]), (boxes[c][1]), (boxes[c][2]), (boxes[c][3]))
 
             item = Object()
             item.name = 'Object'
