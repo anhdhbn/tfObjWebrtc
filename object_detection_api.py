ae5c77480e2ede84e425e628f513ecd361aa0965
AnhDH <anhdhbn@gmail.com>
2019-06-29 19:20:25 +0700

    fix 500

--- object_detection_api.py
+++ object_detection_api.py
@@ -92,26 +92,27 @@ def get_objects(image, threshold=0.5):
     print(help(sess.run))
     # Actual detection.
 
-    # (boxes, scores, classes, num) = sess.run(
-        # [detection_boxes, detection_scores, detection_classes, num_detections],
-        # feed_dict={image_tensor: image_np_expanded})
+    print("tensor_dict", [detection_boxes, detection_scores, detection_classes, num_detections])
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
 
-    ops = tf.get_default_graph().get_operations()
-    all_tensor_names = {output.name for op in ops for output in op.outputs}
-    tensor_dict = {}
-    for key in [
-        'num_detections', 'detection_boxes', 'detection_scores',
-        'detection_classes'
-    ]:
-      tensor_name = key + ':0'
-      if tensor_name in all_tensor_names:
-        tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
-            tensor_name)
+    # ops = tf.get_default_graph().get_operations()
+    # all_tensor_names = {output.name for op in ops for output in op.outputs}
+    # tensor_dict = {}
+    # for key in [
+    #     'num_detections', 'detection_boxes', 'detection_scores',
+    #     'detection_classes'
+    # ]:
+    #   tensor_name = key + ':0'
+    #   if tensor_name in all_tensor_names:
+    #     tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+    #         tensor_name)
 
     # image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
-    print("tensor_dict", tensor_dict)
-    (boxes, scores, classes, num) = sess.run(tensor_dict, feed_dict={image_tensor: image_np_expanded})
+    
+    # (boxes, scores, classes, num) = sess.run(tensor_dict, feed_dict={image_tensor: image_np_expanded})
 
     print("boxes, scores, classes, num", boxes, scores, classes, num)
     classes = np.squeeze(classes).astype(np.int32)
