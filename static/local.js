2a25c8760c031cd04697ba1cf11f77b9b5c91295
AnhDH <anhdhbn@gmail.com>
2019-06-29 17:56:31 +0700

    switch camera

--- static/local.js
+++ static/local.js
@@ -2,16 +2,54 @@
 const constraints = {
     audio: false,
     video: {
-        width: {min: 640, ideal: 1280, max: 1920},
-        height: {min: 480, ideal: 720, max: 1080}
+        width: { min: 640, ideal: 1280, max: 1920 },
+        height: { min: 480, ideal: 720, max: 1080 }
     }
 };
 
-let flipBtn = document.querySelector('#flip-btn');
-let supports = navigator.mediaDevices.getSupportedConstraints();
-if( supports['facingMode'] === true ) {
-  flipBtn.disabled = false;
+const videoSelect = document.querySelector('select#videoSource');
+const selectors = [videoSelect];
+
+function gotDevices(deviceInfos) {
+    // Handles being called several times to update labels. Preserve values.
+    const values = selectors.map(select => select.value);
+    selectors.forEach(select => {
+        while (select.firstChild) {
+            select.removeChild(select.firstChild);
+        }
+    });
+    for (let i = 0; i !== deviceInfos.length; ++i) {
+        const deviceInfo = deviceInfos[i];
+        const option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+        if (deviceInfo.kind === 'videoinput') {
+            option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+            videoSelect.appendChild(option);
+        } else {
+            console.log('Some other kind of source/device: ', deviceInfo);
+        }
+    }
+    selectors.forEach((select, selectorIndex) => {
+        if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
+            select.value = values[selectorIndex];
+        }
+    });
 }
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+function gotStream(stream) {
+    window.stream = stream; // make stream available to console
+    videoElement.srcObject = stream;
+    // Refresh button list in case labels have become available
+    return navigator.mediaDevices.enumerateDevices();
+}
+
+function handleError(error) {
+    console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
 
 navigator.mediaDevices.getUserMedia(constraints)
     .then(stream => {
